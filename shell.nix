710109d53a5b66dd82ae17a00aa8b9a2ab898594
Sam Craig <contact@samcraig.io>
2020-06-17 15:45:28 -0400

    fix: added helm and python to shell.nix

--- shell.nix
+++ shell.nix
@@ -9,7 +9,9 @@ nixpkgs.mkShell rec {
   name = "neptune";
   env = nixpkgs.buildEnv { name = name; paths = buildInputs; };
   buildInputs = with lunarispkgs; [
+    helm_3_2_1
     nodejs_10_19_0
+    python_3_7_7
     skaffold_1_10_1
   ];
 }
